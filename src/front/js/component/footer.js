acb2962dde6536c581e992bbcacb8bb7d54907b7
Sandra Madarnas <sandra.madarnas.1@gmail.com>
2023-05-09 17:57:06 +0000

    Proyecto Finalizado

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by Guille
+			Made with <i className="fa fa-heart text-danger" /> by Sandra
 		</p>
 	</footer>
 );
